d5074d7bda27d8095874d174732168eceb1d9f9c
ortizal19009 <ort19009@byui.edu>
2025-01-16 00:08:10 -0500

    update reportes jspdf autotable

--- amon_ra_fe/src/app/app.routes.ts
+++ amon_ra_fe/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AppWrapperComponent } from './layout/app-wrapper/app-wrapper.component'
 import { ProyectosComponent } from './componentes/contabilidad/proyectos/proyectos.component';
 import { AddProyectoComponent } from './componentes/contabilidad/proyectos/add-proyecto/add-proyecto.component';
 import { ModiProyectoComponent } from './componentes/contabilidad/proyectos/modi-proyecto/modi-proyecto.component';
+import { ImpProyectoComponent } from './componentes/contabilidad/proyectos/imp-proyecto/imp-proyecto.component';
 
 export const routes: Routes = [
   { path: 'inicio', component: AppWrapperComponent },
   { path: 'proyectos', component: ProyectosComponent },
   { path: 'add-proyecto', component: AddProyectoComponent },
   { path: 'modi-proyecto/:idproyecto', component: ModiProyectoComponent },
+  { path: 'imp-proyecto', component: ImpProyectoComponent},
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
 ];
