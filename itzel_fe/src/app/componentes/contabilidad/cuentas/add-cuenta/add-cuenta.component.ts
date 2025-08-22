904c70b6d0b6c73a6fc5ffeb4c0e3912fbe4058e
ortizal19009 <ort19009@byui.edu>
2025-08-22 00:21:07 -0500

    Update Add cuentas

--- itzel_fe/src/app/componentes/contabilidad/cuentas/add-cuenta/add-cuenta.component.ts
+++ itzel_fe/src/app/componentes/contabilidad/cuentas/add-cuenta/add-cuenta.component.ts
@@ -9,12 +9,12 @@ import {
 } from '@angular/forms';
 import { ActivatedRoute, Route, Router, RouterLink } from '@angular/router';
 import { CuentasService } from '../../../../servicios/contabilidad/cuentas.service';
-import { Ifinan } from '../../../../modelos/contabilidad/ifinan';
 import { Cuentas } from '../../../../modelos/contabilidad/cuentas';
 import { TiptranService } from '../../../../servicios/contabilidad/tiptran.service';
 import { NivelesService } from '../../../../servicios/contabilidad/niveles.service';
 import { PresupuestoService } from '../../../../servicios/contabilidad/presupuesto.service';
 import Swal from 'sweetalert2';
+import { ServerConfigService } from '../../../../servicios/config/server-config.service';
 
 @Component({
   selector: 'app-add-cuenta',
@@ -24,6 +24,7 @@ import Swal from 'sweetalert2';
 })
 export class AddCuentaComponent implements OnInit {
   idcuenta!: number;
+  stringFilter: string = '';
   _cuenta?: any;
   f_cuenta!: FormGroup;
   title: string = 'Nueva Cuenta';
@@ -35,6 +36,9 @@ export class AddCuentaComponent implements OnInit {
   _codPartida: any = '';
   _tippar?: any;
   _presupuestos?: any;
+  selectedPresupuesto!: any;
+  modalTitle!: string;
+
   constructor(
     private _params: ActivatedRoute,
     private s_cuentas: CuentasService,
@@ -45,6 +49,7 @@ export class AddCuentaComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     this.idcuenta = +this._params.snapshot.paramMap.get('idcuenta')!;
+
     this.f_cuenta = this.fb.group({
       codcue: ['', Validators.required],
       nomcue: ['', Validators.required],
@@ -63,9 +68,32 @@ export class AddCuentaComponent implements OnInit {
     this.getCuentaById(this.idcuenta);
     this.getAllTipTran();
     this.getAllNiveles();
+    this.modalTitle =
+      this._tippar === 2
+        ? 'Partidas asociadas al debe'
+        : 'Partidas asociadas al haber';
   }
+  setFiltro(e: any) {
+    this.stringFilter = e.target.value;
+  }
+  get presupuestosFiltrados() {
+    if (!this.stringFilter) {
+      return this._presupuestos;
+    }
+    return this._presupuestos.filter(
+      (p: any) =>
+        p.nompar.toLowerCase().includes(this.stringFilter.toLowerCase()) ||
+        p.codigo.toString().includes(this.stringFilter)
+    );
+  }
+
   get f() {
     return this.f_cuenta.controls;
+  }
+  resetBusquedaModal(tippar: number) {
+    this._tippar = tippar;
+    this.stringFilter = '';
+    this._presupuestos = [];
   }
   getAllTipTran() {
     this.s_tiptran.getAllTiptran().subscribe({
@@ -89,10 +117,12 @@ export class AddCuentaComponent implements OnInit {
   getCuentaById(idcuenta: number) {
     this.s_cuentas.getByIdCuenta(idcuenta).subscribe({
       next: (datos: any) => {
+        console.log(datos);
         this._cuenta = datos;
         this.f_cuenta.patchValue({
           grucue: datos.grucue,
-          nomcue: datos.nomcue,
+          nomgrup: datos.nomcue,
+          codcue: datos.grucue
         });
       },
       error: (e: any) => console.error(e),
@@ -102,6 +132,7 @@ export class AddCuentaComponent implements OnInit {
   getValidacionCodigo(e: any) {}
   getAsoDebeHaber() {
     console.log(this._codPartida);
+
     this.s_presupuestos
       .findTipparAndCodpar(this._tippar, this._codPartida)
       .subscribe({
@@ -115,6 +146,21 @@ export class AddCuentaComponent implements OnInit {
         },
         error: (e: any) => console.error(e),
       });
+  }
+  selectPresupuesto(presupuesto: any) {
+    this.selectedPresupuesto = presupuesto; // guarda el seleccionado
+    console.log('Seleccionado:', presupuesto);
+  }
+  setPresupuesto() {
+    let f = this.f_cuenta;
+    if (this._tippar === 1) {
+      f.patchValue({ asohaber: this.selectedPresupuesto.codigo });
+    }
+    if (this._tippar === 2) {
+      f.patchValue({ asodebe: this.selectedPresupuesto.codigo });
+    }
+
+    console.log(this.selectedPresupuesto);
   }
 
   save() {
