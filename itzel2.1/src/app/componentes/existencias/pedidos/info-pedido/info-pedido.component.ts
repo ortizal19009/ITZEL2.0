d40bdf5a0f1c2f68cba80b042c520081b6cb38fe
ortizal19009 <ort19009@byui.edu>
2025-10-18 12:29:17 -0500

    update

---
+++ itzel2.1/src/app/componentes/existencias/pedidos/info-pedido/info-pedido.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-pedido.component',
+  imports: [],
+  templateUrl: './info-pedido.component.html',
+  styleUrl: './info-pedido.component.css'
+})
+export class InfoPedidoComponent {
+
+}
